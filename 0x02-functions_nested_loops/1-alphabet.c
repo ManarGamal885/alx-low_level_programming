aa8faad4db8b94cc3cfb61984a47b6d5c6ad20f1
ManarGamal885 <manarg281@gmail.com>
2023-03-21 06:09:05 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
-void print_alphabet(void){
+void print_alphabet(void)
+{
     for (char i = 'a'; i <= 'z'; i++)
     {
         _putchar(i);
         _putchar('\n');
     }
-    
 }
