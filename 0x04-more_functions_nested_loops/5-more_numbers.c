fd2ef7be2ed216f7ef911e434e08b14adca5fdb2
Manar Gamal <60050380+ManarGamal885@users.noreply.github.com>
2023-03-25 04:09:06 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,19 @@
  */
 void more_numbers(void)
 {
- int i, j;
+	int i;
+	int num = 0;
 
- for (i = 0; i < 10; i++)
- {
- for (j = 0; j < 15; j++)
- {
- if (j >= 10)
- _putchar(j / 10 + '0');
- _putchar(j % 10 + '0');
- }
- _putchar('\n');
- }
+	for (i = 0 ; i < 10 ; i++)
+	{
+		while (num < 15)
+		{
+			if (num > 9)
+				_putchar('0' + num / 10);
+			_putchar('0' + num % 10);
+			num++;
+		}
+		num = 0;
+		_putchar('\n');
+	}
 }
-
